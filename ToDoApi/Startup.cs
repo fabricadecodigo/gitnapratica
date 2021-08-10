e661b08acf1f376ee0b08843a2344973ff948281
Felipe Franco <felipe@fabricadecodigo.com>
2021-08-10 20:26:15 -0300

    modificacao2

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoApi.src.Api;
